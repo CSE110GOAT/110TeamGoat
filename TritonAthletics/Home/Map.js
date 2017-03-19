a986014f3cb5c27418a739331e2e4c90455d2e35
Unknown <nikbose97@gmail.com>
2017-03-18 21:08:27 -0700

    Added in the Home page

---
+++ TritonAthletics/Home/Map.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { MapView, AppRegistry, View, Text } from 'react-native';
+
+export default class Maps extends Component {
+
+
+
+  state = {
+    region: {
+      latitude: parseFloat(this.props.latitude),
+      longitude: parseFloat(this.props.longitude),
+      latitudeDelta: 1,
+      longitudeDelta: 1
+    },
+    annotations: [{latitude: parseFloat(this.props.latitude), longitude: parseFloat(this.props.longitude), animateDrop: true}]
+  };
+
+  render() {
+    return (
+      <View style={{flex:1}}>
+      <Text style={{textAlign: 'center'}}>{this.props.loc}</Text>
+      <MapView
+        style={{flex:1}}
+        region={this.state.region}
+        annotations={this.state.annotations}
+      />
+      </View>
+    );
+  }
+}
+
+AppRegistry.registerComponent('Maps', () => Maps);
